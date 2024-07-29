bd712efc833cbfc1600052b1ce33e21e1730e7e4
Gustavo Rocha <gurocha05@gmail.com>
2024-07-29 20:50:06 -0300

    grade

--- 2-javascript_tant/test_6/type_number.js
+++ 2-javascript_tant/test_6/type_number.js
@@ -1,10 +1,12 @@
-const noteFirtsBi = 8;
-const noteSecondBi= 6.3;
-const noteThirdBi= -3;
-const noteFinalBi= '5';
+const sFirtsBi = 8;
+const sSecondBi= 7.5;
+const sThirdBi= -0.5;
+const sFinalBi= Number.parseInt('9');
 
-const total = noteFirtsBi + noteSecondBi+ noteThirdBi
- + noteFinalBi;
+ const total = (sFirtsBi + sSecondBi+ sThirdBi
++ sFinalBi) / 4;
 
-console.log(total);
+// console.log(total);
 
+const name = 'Gustavo';
+console.log('My name is ' + name + ', and my grade is ' + total)
