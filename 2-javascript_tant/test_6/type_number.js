d48eb4dcdf3db259a6e31b4f0d7a0af9ae496789
Gustavo Rocha <gurocha05@gmail.com>
2024-07-29 20:39:10 -0300

    type_number

---
+++ 2-javascript_tant/test_6/type_number.js
@@ -0,0 +1,10 @@
+const noteFirtsBi = 8;
+const noteSecondBi= 6.3;
+const noteThirdBi= -3;
+const noteFinalBi= '5';
+
+const total = noteFirtsBi + noteSecondBi+ noteThirdBi
+ + noteFinalBi;
+
+console.log(total);
+
