12b2c26047c32ef61a03bb152bf8d417f7a8baab
Gustavo Rocha <gurocha05@gmail.com>
2024-07-30 19:34:55 -0300

    nan2

--- 2-javascript_tant/test_7/nan.js
+++ 2-javascript_tant/test_7/nan.js
@@ -2,3 +2,11 @@ console.log(5 * 'a'); //NaN
 
 console.log(5 * '5');
 
+const numero = 10;
+const nome = 'Ana';
+
+// expl
+
+Number.isNaN(numero) // false
+Number.isNaN(nome) // false
+Number.isNaN(NaN) // true
