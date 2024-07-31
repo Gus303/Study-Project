6fa09a7709bfddb9621b44bc6201149777657c6a
Gustavo Rocha <gurocha05@gmail.com>
2024-07-31 17:01:24 -0300

    string

---
+++ 2-javascript_tant/test_8/string.js
@@ -0,0 +1,15 @@
+const student = "Gustavo";
+const teacher = 'Caroline';
+const compliment = "They are always so 'Smart'";
+const quote = 'He used to say: "Be kind with everyone"';
+
+console.log(compliment);
+console.log(quote);
+
+console.log('Hi, my name is ' + student)
+// or 
+console.log(`Hi, my name is ${student}`);
+
+const password = '1234@' + student.toUpperCase();
+
+console.log(password);
