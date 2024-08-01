364155a7657c07fabc8f3391cef6bc31ad74e75f
Gustavo Rocha <gurocha05@gmail.com>
2024-08-01 20:20:57 -0300

    ;

--- 2-javascript_tant/test_8/utf.js
+++ 2-javascript_tant/test_8/utf.js
@@ -1,9 +1,9 @@
-const dollarSign = '\u0024'
-const lCALEA = '\u0041'
-const checkMark = '\u2705'
-const hiragana = '\u3041'
+const dollarSign = '\u0024';
+const lCALEA = '\u0041';
+const checkMark = '\u2705';
+const hiragana = '\u3041';
 
-console.log(dollarSign)
-console.log(lCALEA)
-console.log(checkMark)
-console.log(hiragana)
+console.log(dollarSign);
+console.log(lCALEA);
+console.log(checkMark);
+console.log(hiragana);
