c890814ef8b8d9520e19e8aca22f787b2916edc4
Gustavo Rocha <gurocha05@gmail.com>
2024-08-05 19:49:21 -0300

    boolean

---
+++ 2-javascript_tant/test_8/boolean.js
@@ -0,0 +1,19 @@
+const student = 'Gustavo';
+const approved = true;
+//const approved = false;
+
+if(approved === true) {
+    console.log('congratuletions!')
+}
+else{
+    console.log('sorry :(')
+}
+
+if (student === 'Gustavo')
+//if (student === 'Someone')    
+{
+    console.log(`Hello, ${student}`)
+}
+else{
+ console.log('Who are you ?')
+}
