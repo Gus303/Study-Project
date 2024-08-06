852e940cae9f32f8ddb3b1ba28e3191aa6218b97
Gustavo Rocha <gurocha05@gmail.com>
2024-08-05 21:11:19 -0300

    string.js

---
+++ 2-javascript_tant/test_9/string.js
@@ -0,0 +1,10 @@
+let valueString = '10';
+let valueNumber = 5;
+
+let result = valueString + valueNumber;
+console.log(result);
+
+let value_String = '20';
+let value_Number = parseInt(value_String);
+
+console.log(value_Number);
