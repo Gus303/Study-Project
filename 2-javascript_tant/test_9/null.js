0dbaea2aa6a5477ab8bb8490738168f29e0cf777
Gustavo Rocha <gurocha05@gmail.com>
2024-08-08 17:29:24 -0300

    null

---
+++ 2-javascript_tant/test_9/null.js
@@ -0,0 +1,11 @@
+let nameStudent;
+
+console.log(nameStudent);
+console.log(typeof nameStudent);
+
+let phoneStudent = null;
+
+console.log(phoneStudent);
+
+console.log(phoneStudent + 3);
+console.log(nameStudent + 3);
