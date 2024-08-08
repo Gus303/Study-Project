ce66fee7d3a5a62f325d872abe3527481dee0b28
Gustavo Rocha <gurocha05@gmail.com>
2024-08-08 17:55:58 -0300

    typeof

---
+++ 2-javascript_tant/test_9/typeoff.js
@@ -0,0 +1,18 @@
+const string = 'string';
+const number = 3;
+let undefined;
+var boolean = true;
+Math.sin
+new Date();
+1234n
+Symbol()
+let object = null; // this is a JavaScript error
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof undefined);
+console.log(typeof boolean);
+console.log(typeof Date);
+console.log(typeof 1234n);
+console.log(typeof Symbol())
+console.log(typeof object);
